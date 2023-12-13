57bb0ec38a7207a1cc613e03b5e7c4f8a4e35e4b
VD2401 <102732099+VD2401@users.noreply.github.com>
2023-12-13 18:50:59 +0100

    Reorder function testfile.

--- tests/function_test.cpp
+++ tests/function_test.cpp
@@ -6,16 +6,23 @@
 
 namespace{
 
+class FunctionTest : public ::testing::Test {
+protected:
+    // Define the function
+    Function<5, 5>* f;
 
-class PolynomialTest : public ::testing::Test {
+    virtual void SetUp() = 0;
+    virtual void TearDown() = 0;
+};
+
+class PolynomialTest : public FunctionTest {
 protected:
     // Define the coefficients of the polynomial
-    Polynomial<1, 1>* f;
     std::vector<double>* coeffs;
 
     virtual void SetUp() override{
         coeffs = new std::vector<double> {1., 2., 3., 4., 5., 6., 7., 8.};
-        f = new Polynomial<1, 1>(*coeffs);
+        f = new Polynomial<5, 5>(*coeffs);
     };
     virtual void TearDown() override{
         delete f;
@@ -23,25 +30,22 @@ protected:
     };
 };
 
-TEST_F(PolynomialTest, PolynomialEval1D)
+TEST_F(PolynomialTest, PolynomialEval)
 {
     // Check that the polynomial is evaluated correctly
-    for (int i = 0; i < 10; ++i) {
+    for (double i = 0.; i < 10.; i+=0.1) {
         // Define the input vector
-        Vector<1> x{i}; 
-
+        Vector<5> x = std::vector<double>{i, i + 1, i + 2, i + 3, i + 4};
         // Evaluate the polynomial
-        Vector<1> y = (*f)(x);
-
+        Vector<5> y = f->call(x);
         // Evaluate the polynomial manually
-        Vector<1> y2(0.);
+        Vector<5> y_manual = 0.;
         for (int j = 0; j < coeffs->size(); ++j) {
-            y2[0] += (*coeffs)[j] * std::pow(x[0], j); 
+            y_manual += (*coeffs)[j] * (x ^ j);
         }
-
-        // We use exact evaluation of double because 
-        // the exact same operations are supposed to be performed
-        EXPECT_EQ(y[0], y2[0]); 
+        for (int j = 0; j < 5; ++j) {
+            EXPECT_DOUBLE_EQ(y[j], y_manual[j]);
+        }
     }
 }
 
